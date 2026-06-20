e5c27039de3f654b3bb2d4e3704df9cbd09ee2de
CaptainFallaway <alexander.wollmer97@gmail.com>
2026-06-20 20:51:26 +0200

    Added a small package for a map based hash set implementation

---
+++ pkg/hashset/hashset.go
@@ -0,0 +1,31 @@
+package hashset
+
+type Set[T comparable] map[T]struct{}
+
+func NewSet[T comparable](is ...T) Set[T] {
+	if len(is) == 0 {
+		return make(Set[T])
+	}
+	s := make(Set[T], len(is))
+	for _, i := range is {
+		s.Add(i)
+	}
+	return s
+}
+
+func (s Set[T]) Add(i T) {
+	s[i] = struct{}{}
+}
+
+func (s Set[T]) Remove(i T) {
+	delete(s, i)
+}
+
+func (s Set[T]) Contains(i T) bool {
+	_, ok := s[i]
+	return ok
+}
+
+func (s Set[T]) Size() int {
+	return len(s)
+}
